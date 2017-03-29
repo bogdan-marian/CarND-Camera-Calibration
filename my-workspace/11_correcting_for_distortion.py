e23d0c2092da77ded2beaedb9f36b3c02b4e2a18
Bogdan Oloeriu <bogdan.oloeriu@gmail.com>
2017-03-30 01:51:56 +0200

    calibrating and undistorting a bunch of immages one by one

--- my-workspace/11_correcting_for_distortion.py
+++ my-workspace/11_correcting_for_distortion.py
@@ -3,10 +3,11 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import glob
 
 # Read in a calibration image
-img = mpimg.imread('../calibration_wide/GOPR0032.jpg')
-plt.imshow(img)
+images  = glob.glob('../calibration_wide/GO*.jpg')
+#plt.imshow(img)
 #plt.show()
 
 # Arrays to store object points and image points from all the images
@@ -17,17 +18,28 @@ imgpoints = [] #2d points in image plane
 objp = np.zeros((6*8, 3), np.float32)
 objp[:,:2]  = np.mgrid[0:8, 0:6].T.reshape(-1,2) #x, y coordinates
 
-# Convert image to grayscale
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+for fname in images:
+    # read in each image
 
-# Find the chessboard corners
-ret, corners = cv2.findChessboardCorners(gray, (8,6), None)
-# If corners are found, add object points, image points
-if ret == True:
-    imgpoints.append(corners)
-    objpoints.append(objp)
+    img = mpimg.imread(fname)
 
-    # draw and sisplay the corners
-    img = cv2.drawChessboardCorners(img, (8,6), corners, ret)
-    plt.imshow(img)
-    plt.show()
+    # Convert image to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Find the chessboard corners
+    ret, corners = cv2.findChessboardCorners(gray, (8,6), None)
+    # If corners are found, add object points, image points
+    if ret == True:
+        imgpoints.append(corners)
+        objpoints.append(objp)
+
+        # draw and sisplay the corners
+        img = cv2.drawChessboardCorners(img, (8,6), corners, ret)
+        plt.imshow(img)
+        plt.show()
+
+        # calibrate camera for this image
+        ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+        undistorted = cv2.undistort(img, mtx, dist, None, mtx)
+        plt.imshow(undistorted)
+        plt.show()
